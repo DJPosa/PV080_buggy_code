ec376f1c62d62fb3f76ca069cece052baf89232f
unknown <xpospi13@ad.fi.muni.cz>
2023-03-27 12:53:36 +0200

    no. 1

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -12,7 +12,7 @@ def index():
     url = flask.request.args.get("url")
     return fetch_website(version, url)
 
-        
+
 CONFIG = {"API_KEY": "771df488714111d39138eb60df756e6b"}
 class Person(object):
     def __init__(self, name):
@@ -39,11 +39,13 @@ def load_yaml(filename):
     stream = open(filename)
     deserialized_data = yaml.load(stream, Loader=yaml.Loader) #deserializing data
     return deserialized_data
-    
+
+
 def authenticate(password):
     # Assert that the password is correct
     assert password == "Iloveyou", "Invalid password!"
     print("Successfully authenticated!")
+
 
 if __name__ == '__main__':
     print("Vulnerabilities:")
@@ -53,15 +55,14 @@ if __name__ == '__main__':
     print("4. Use of assert statements vulnerability: run program with -O argument")
     choice  = input("Select vulnerability: ")
     if choice == "1": 
-        new_person = Person("Vickie")  
-        print_nametag(input("Please format your nametag: "), new_person)
+        new_person = Person("Vickie")
+        print_nametag(raw_input("Please format your nametag: "), new_person)
     elif choice == "2":
-        urlib_version = input("Choose version of urllib: ")
+        urlib_version = raw_input("Choose version of urllib: ")
         fetch_website(urlib_version, url="https://www.google.com")
     elif choice == "3":
-        load_yaml(input("File name: "))
+        load_yaml(raw_input("File name: "))
         print("Executed -ls on current folder")
     elif choice == "4":
-        password = input("Enter master password: ")
+        password = raw_input("Enter master password: ")
         authenticate(password)
-
